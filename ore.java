c74b3ce62e0b9de5158d344e8ee3a4e0179d5188
vithyabharathi <vithyavb17@gmail.com>
2016-08-09 18:42:35 +0530

    Create ore.java

---
+++ ore.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ore
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner scr=new Scanner(System.in);
+		n=scr.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
