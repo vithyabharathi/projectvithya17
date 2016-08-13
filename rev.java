c54576926c409f92e102429e6f3344ef8d759ba1
vithyabharathi <vithyavb17@gmail.com>
2016-08-13 11:04:40 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class rev
+{
+	public static void main(String args[])
+	{
+     String s;
+     Scanner scr=new Scanner(System.in);
+     s=scr.next();
+     if(s.length()>1)
+     {
+     StringBuffer sb=new StringBuffer(s);
+     System.out.println(sb.reverse());
+     }
+     else
+     {
+     	System.out.println("not given enough string to reverse");
+     }
+	}
+}
