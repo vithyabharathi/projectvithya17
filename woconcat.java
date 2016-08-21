c95c575d59d8f350479bb61b9c3493160aa8cc0a
vithyabharathi <vithyavb17@gmail.com>
2016-08-21 15:22:33 +0530

    Create woconcat.java

---
+++ woconcat.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class woconcat
+{
+	public static void main(String args[])
+	{
+		String s1;
+		String s2;
+		Scanner scr=new Scanner(System.in);
+		s1=scr.next();
+		s2=scr.next();
+		System.out.println(s1+s2);
+	}
+}
