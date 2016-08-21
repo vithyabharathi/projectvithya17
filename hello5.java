852bcc4c5905c7e1387161d5391243f07302b08a
vithyabharathi <vithyavb17@gmail.com>
2016-08-21 13:26:16 +0530

    Create hello5.java

---
+++ hello5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class hello5
+{
+	public static void main(String args[])
+	{
+		String s="hello";
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(s);
+		}
+	}
+}
