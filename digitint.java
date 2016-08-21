00f0a9a668a6eef79758ecbe0fbccbfb04748924
vithyabharathi <vithyavb17@gmail.com>
2016-08-21 13:32:46 +0530

    Create digitint.java

---
+++ digitint.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class digitint
+{
+public static void main(String args[])
+{
+	int n;
+	Scanner scr=new Scanner(System.in);
+	n=scr.nextInt();
+	String s=String.valueOf(n);
+	System.out.println(s.length());
+}
+}
