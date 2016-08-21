f81b245dfda54e031a69c83cedd0e0cdb7076ebf
vithyabharathi <vithyavb17@gmail.com>
2016-08-21 15:52:44 +0530

    Create secondarray.java

---
+++ secondarray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+class secondarray
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner scr=new Scanner(System.in);
+		n=scr.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=scr.nextInt();
+		}
+		int t=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+				}
+			}
+		}
+	System.out.println(a[1]);
+	}
+}
